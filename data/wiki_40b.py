4d4904619972e4c8aa109c3b1d8ffb939bc89e91
Thomas Clark <thclark@mit.edu>
2022-01-20 10:53:40 -0500

    changed counting method from num examples to num tokens

--- data/wiki_40b.py
+++ data/wiki_40b.py
@@ -13,34 +13,38 @@ r4 = "_NEWLINE_"
 REGEX = re.compile(f"({r1}|{r2}|{r3}|{r4})")
 
 
-def process_tf_dataset(ds, num_examples, output_file):
+def process_tf_dataset(ds, num_tokens, output_file):
     # Turn to a numpy df so that we can easily extract text
     numpy_items = tfds.as_numpy(ds)
+    token_count = 0
 
     with open(output_file, "a") as f:
-        for _, item in zip(range(num_examples), numpy_items):
+        for item in numpy_items:
             text = item.get("text").decode("UTF-8")
             text = re.sub(REGEX, " ", text)
             text = re.sub("\s+", " ", text).strip()
             f.write(text)
             f.write("\n")
+            token_count += len(text.split())
+            if token_count > num_tokens:
+                break
 
 
 def process_lang(lang_code, args):
     # Construct a tf.data.Dataset
     ds_train = tfds.load(f"wiki40b/{lang_code}", split="train", shuffle_files=True)
     process_tf_dataset(
-        ds_train, args.num_train_examples, args.output_prefix + lang_code + ".train"
+        ds_train, args.num_train_tokens, args.output_prefix + lang_code + ".train"
     )
 
     ds_test = tfds.load(f"wiki40b/{lang_code}", split="test", shuffle_files=True)
     process_tf_dataset(
-        ds_test, args.num_test_examples, args.output_prefix + lang_code + ".test"
+        ds_test, args.num_test_tokens, args.output_prefix + lang_code + ".test"
     )
 
     ds_valid = tfds.load(f"wiki40b/{lang_code}", split="validation", shuffle_files=True)
     process_tf_dataset(
-        ds_valid, args.num_valid_examples, args.output_prefix + lang_code + ".valid"
+        ds_valid, args.num_valid_tokens, args.output_prefix + lang_code + ".valid"
     )
 
 
@@ -51,21 +55,21 @@ if __name__ == "__main__":
         help="comma-separated list of language codes to use, e.g. 'en,de,nl,hu'",
     )
     parser.add_argument(
-        "--num_train_examples",
-        type=int,
-        default=80,
+        "--num_train_tokens",
+        type=float,
+        default=8e5,
         help="max number of training examples to sample",
     )
     parser.add_argument(
-        "--num_test_examples",
-        type=int,
-        default=10,
+        "--num_test_tokens",
+        type=float,
+        default=1e5,
         help="max number of test examples to sample",
     )
     parser.add_argument(
-        "--num_valid_examples",
-        type=int,
-        default=10,
+        "--num_valid_tokens",
+        type=float,
+        default=1e5,
         help="max number of validation examples to sample",
     )
     parser.add_argument(
