78e964e43e7a2617398592aa9a16228fb73ef6d9
Thomas Clark <thclark@mit.edu>
2022-02-24 21:20:44 -0500

    first attempt at hyperparam optimization

---
+++ data/train_models_hypopt.sh
@@ -0,0 +1,13 @@
+default=data-bin
+ALL_DATA="${1:-$default}"
+ALL_CHECKPOINTS="checkpoints_hypopt"
+LOG_DIR="logs_hypopt"
+
+langlist=("data-bin/ar" "data-bin/ar-rev" "data-bin/da" "data-bin/da-rev" "data-bin/el" "data-bin/el-rev" "data-bin/he" "data-bin/he-rev" "data-bin/hu" "data-bin/hu-rev" "data-bin/ko" "data-bin/ko-rev" "data-bin/sr" "data-bin/sr-rev" "data-bin/sv" "data-bin/sv-rev" "data-bin/th" "data-bin/th-rev")
+
+langlist=("data-bin/en" "data-bin/en-rev")
+for D in "${langlist[@]}" ; do
+	prefix=$(basename $D |  cut -d '.' -f1)
+	echo $D $ALL_CHECKPOINTS/$prefix
+	bsub -W 24:00 -o $LOG_DIR/$prefix.out -R "select[gpu_mtotal0>=20480]"  -R "rusage[mem=30000,ngpus_excl_p=1]" bash train_model_transformer.sh $D $ALL_CHECKPOINTS/$prefix
+done
