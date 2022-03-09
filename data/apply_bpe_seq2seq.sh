daee55d29969e48d38d40760e509caf39d959477
Thomas Clark <thclark@mit.edu>
2022-03-09 12:36:46 -0500

    applying bpe seq2seq

--- data/apply_bpe_seq2seq.sh
+++ data/apply_bpe_seq2seq.sh
@@ -1,8 +1,8 @@
 BPE_CODES=bpe_codes/30k  # path where processed files will be stored
 FASTBPE=../fastBPE/fast  # path to the fastBPE tool
-INPUT_DIR="wiki40b-txt-final-doc"
+INPUT_DIR="wiki40b-txt-final-seq2seq"
 INPUT_DIR_REV=$INPUT_DIR-rev
-OUT_DIR="wiki40b-txt-final-doc-bpe"
+OUT_DIR="wiki40b-txt-final-seq2seq-bpe"
 OUT_DIR_REV=$OUT_DIR-rev
 
 # create output path
@@ -10,7 +10,7 @@ mkdir -p $OUT_DIR $OUT_DIR_REV
 
 # langlist=("ar" "bg" "ca" "cs" "de" "el" "es" "et" "fa" "fi" "fr" "he" "hi" "hr" "id" "it" "ko" "lt" "lv" "ms" "nl" "no" "pl" "pt" "ro" "ru" "sk" "sl" "sr" "sv" "th" "tl" "tr" "uk" "vi" "zh-cn")
 langlist=("en" "de" "fr" "ru" "vi")
-extlist=("train" "test" "valid")
+extlist=("train.1" "train.2" "test.1" "test.2" "valid.1" "valid.2")
 
 for lang in "${langlist[@]}"
 do
