b39b545bf0c3cc5f1f46f67fe8d961860eb25a08
Thomas Clark <thclark@mit.edu>
2022-03-09 20:36:25 -0500

    dataset generation issue

--- data/tokenization-seq2seq.sh
+++ data/tokenization-seq2seq.sh
@@ -3,10 +3,11 @@
 #langlist=("ar" "bg" "ca" "cs" "da" "de" "el" "en" "es" "et" "fa" "fi" "fr" "he" "hi" "hr" "id" "it" "ko" "lt" "lv" "ms" "nl" "no" "pl" "pt" "ro" "ru" "sk" "sl" "sr" "sv" "th" "tl" "tr" "uk" "vi" "zh-cn")
 langlist=("ru" "vi" "en" "de" "fr")
 extlist=("train" "test" "valid")
+suff="x"
 
 input_dir="wiki40b-txt-normalized"
-tmp_dir="wiki40b-txt-tokenized-seq2seq"
-tmp_dir_rev=$tmp_dir-rev
+# tmp_dir="wiki40b-txt-tokenized-seq2seq"
+# tmp_dir_rev=$tmp_dir-rev
 output_dir="wiki40b-txt-final-seq2seq"
 output_dir_rev=$output_dir-rev
 
@@ -18,21 +19,21 @@ do
     echo "Processing $lang"
     for ext in "${extlist[@]}"
     do
-        # python pytokenize.py --in_file $input_dir/$lang.$ext --out_file $tmp_dir/$lang.$ext.1 --out_file2 $tmp_dir/$lang.$ext.2 --language $lang --seq2seq
-        # python pytokenize.py --in_file $input_dir/$lang.$ext --out_file $tmp_dir_rev/$lang.$ext.1 --out_file2 $tmp_dir_rev/$lang.$ext.2 --language $lang --reverse --seq2seq
+        python pytokenize.py --in_file $input_dir/$lang.$ext --out_file $output_dir/$ext.$lang --out_file2 $output_dir/$ext.$lang$suff --language $lang --seq2seq
+        python pytokenize.py --in_file $input_dir/$lang.$ext --out_file $output_dir_rev/$ext.$lang --out_file2 $output_dir_rev/$ext.$lang$suff --language $lang --reverse --seq2seq
 	echo "..."
     done
 done
 
-printf -v joined_langlist '%s,' "${langlist[@]}"
-python sample.py \
-    --lang_code_list "${joined_langlist%,}" \
-    --input_prefix $tmp_dir \
-    --output_prefix $output_dir \
-    --ext_list "train.1,train.2,test.1,test.2,valid.1,valid.2"
+# printf -v joined_langlist '%s,' "${langlist[@]}"
+# python sample.py \
+#     --lang_code_list "${joined_langlist%,}" \
+#     --input_prefix $tmp_dir \
+#     --output_prefix $output_dir \
+#     --ext_list "train.1,train.2,test.1,test.2,valid.1,valid.2"
 
-python sample.py \
-    --lang_code_list "${joined_langlist%,}" \
-    --input_prefix $tmp_dir_rev \
-    --output_prefix $output_dir_rev \
-    --ext_list "train.1,train.2,test.1,test.2,valid.1,valid.2"
+# python sample.py \
+#     --lang_code_list "${joined_langlist%,}" \
+#     --input_prefix $tmp_dir_rev \
+#     --output_prefix $output_dir_rev \
+#     --ext_list "train.1,train.2,test.1,test.2,valid.1,valid.2"
