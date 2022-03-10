c9c4761b43748543faa18dee2463f0c66c38ea74
Thomas Clark <thclark@mit.edu>
2022-03-09 21:21:11 -0500

    apply bpe and preprocess

--- data/apply_bpe_seq2seq.sh
+++ data/apply_bpe_seq2seq.sh
@@ -10,15 +10,18 @@ mkdir -p $OUT_DIR $OUT_DIR_REV
 
 # langlist=("ar" "bg" "ca" "cs" "de" "el" "es" "et" "fa" "fi" "fr" "he" "hi" "hr" "id" "it" "ko" "lt" "lv" "ms" "nl" "no" "pl" "pt" "ro" "ru" "sk" "sl" "sr" "sv" "th" "tl" "tr" "uk" "vi" "zh-cn")
 langlist=("en" "de" "fr" "ru" "vi")
-extlist=("train.1" "train.2" "test.1" "test.2" "valid.1" "valid.2")
+extlist=("train" "test" "valid")
+suff="x"
 
 for lang in "${langlist[@]}"
 do
     echo $lang
     for ext in "${extlist[@]}"
     do
-	$FASTBPE applybpe $OUT_DIR/$lang.$ext $INPUT_DIR/$lang.$ext  $BPE_CODES/$lang.codes
-	#$FASTBPE applybpe $OUT_DIR_REV/$lang.$ext  $INPUT_DIR_REV/$lang.$ext $BPE_CODES/$lang.codes
+	$FASTBPE applybpe $OUT_DIR/$ext.$lang $INPUT_DIR/$ext.$lang  $BPE_CODES/$lang.codes
+	$FASTBPE applybpe $OUT_DIR_REV/$ext.$lang  $INPUT_DIR_REV/$ext.$lang $BPE_CODES/$lang.codes
+    $FASTBPE applybpe $OUT_DIR/$ext.$lang$suff $INPUT_DIR/$ext.$lang$suff  $BPE_CODES/$lang.codes
+	$FASTBPE applybpe $OUT_DIR_REV/$ext.$lang$suff  $INPUT_DIR_REV/$ext.$lang$suff $BPE_CODES/$lang.codes
 done
 done
 
--- data/preprocess_data_seq2seq.sh
+++ data/preprocess_data_seq2seq.sh
@@ -4,27 +4,27 @@ valid_pref="valid"
 test_pref="test"
 
 # for D in $(find $data_dir -mindepth 1 -maxdepth 1 -name "train*") ; do
-langlist=("en")
-langlist2=("enx")
+langlist=("en" "fr" "de" "ru" "vi")
+suff="x"
 
-for i in ${!langlist[@]}
+for lang in ${langlist[@]}
 do
    # prefix=$(basename $D |  cut -d '.' -f1) 
    fairseq-preprocess \
-        --source-lang ${langlist[$i]} \
-        --target-lang ${langlist2[$i]} \
+        --source-lang $lang \
+        --target-lang $lang$suff \
         --trainpref $data_dir/$train_pref \
         --validpref $data_dir/$valid_pref \
         --testpref $data_dir/$test_pref \
-        --destdir data-bin-seq2seq/${langlist[$i]} \
+        --destdir data-bin-seq2seq/$lang \
         --workers 20
    
    fairseq-preprocess \
-        --source-lang ${langlist[$i]} \
-        --target-lang ${langlist2[$i]} \
+        --source-lang $lang \
+        --target-lang $lang$suff \
         --trainpref $data_dir-rev/$train_pref \
         --validpref $data_dir-rev/$valid_pref \
         --testpref $data_dir-rev/$test_pref \
-        --destdir data-bin-seq2seq/${langlist[$i]}-rev \
+        --destdir data-bin-seq2seq/$lang-rev \
 	--workers 20
 done
