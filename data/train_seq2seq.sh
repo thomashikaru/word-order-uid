8917ea52e65601b74ceaf9a8be9c9a1b7f2aa59b
Thomas Clark <thclark@mit.edu>
2022-03-09 20:16:27 -0500

    seq2seq training script

---
+++ data/train_seq2seq.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=0 fairseq-train \
+    data-bin-seq2seq \
+    --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --dropout 0.3 --weight-decay 0.0001 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --max-tokens 4096 \
+    --maximize-best-checkpoint-metric
