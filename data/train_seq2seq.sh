e5f4228ec42c759f61cf66af283d85b1366c65c8
Thomas Clark <thclark@mit.edu>
2022-03-09 20:23:31 -0500

    seq2seq training scripts

--- data/train_seq2seq.sh
+++ data/train_seq2seq.sh
@@ -1,5 +1,9 @@
+DATA_DIR=$1
+SAVE_DIR=$2
+
 CUDA_VISIBLE_DEVICES=0 fairseq-train \
-    data-bin-seq2seq-en \
+    $DATA_DIR \
+    --save-dir $SAVE_DIR \
     --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
