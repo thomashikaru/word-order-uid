eb989c0ae3e09a154a1034a5ce1bf7c82393a38f
Thomas Clark <thclark@mit.edu>
2022-03-14 10:42:13 -0400

    eval script changes

--- data/eval_seq2seq.sh
+++ data/eval_seq2seq.sh
@@ -4,5 +4,6 @@ OUTPUT_DIR=$3
 
 fairseq-generate $DATA_DIR \
     --path $CHECKPOINT_DIR \
-    --batch-size 128 --beam 5 --remove-bpe \
-    --results-path $OUTPUT_DIR
+    --max-tokens 4096 --beam 5 --remove-bpe \
+    --results-path $OUTPUT_DIR \
+    --skip-invalid-size-inputs-valid-test
--- data/train_seq2seq.sh
+++ data/train_seq2seq.sh
@@ -9,7 +9,9 @@ CUDA_VISIBLE_DEVICES=0 fairseq-train \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-    --max-tokens 1024 \
+    --max-tokens 4096 \
     --truncate-source \
+    --max-source-positions 1024 \
+    --max-target-positions 128 \
     --maximize-best-checkpoint-metric \
     --seed $RANDOM_SEED 5
