7e086b702d59db3624bc0434c58ca0bc18e8220c
Thomas Clark <thclark@mit.edu>
2022-06-27 21:16:01 -0400

    train bpe script

--- data/train_bpe_cf.sh
+++ data/train_bpe_cf.sh
@@ -3,6 +3,18 @@ FASTBPE=../fastBPE/fast  # path to the fastBPE tool
 NUM_OPS=30000
 INPUT_DIR="wiki40b-txt-cf"
 
+# Job details
+TIME=01:00  # HH:MM (default: 04:00, max: 240:00)
+NUM_GPUS=0  # GPUs per node
+NUM_CPUS=1  # Number of cores (default: 1)
+CPU_RAM=2048  # RAM for each core (default: 1024)
+ 
+# Load modules
+module load gcc/6.3.0
+module load python_gpu/3.8.5 hdf5 eth_proxy
+module load geos
+module load libspatialindex
+
 # create output path
 mkdir -p $OUTPATH
 
@@ -12,8 +24,12 @@ langlist=("en" "de" "fr" "ru" "vi")
 for lang in "${langlist[@]}"
 do
     echo $lang
-    cat $INPUT_DIR/$lang/*/$lang.train > $lang-agg.txt
+    cat $INPUT_DIR/$lang/*/$lang.train | shuf > $lang-agg.txt
     # learn bpe codes on the training set (or only use a subset of it)
-    $FASTBPE learnbpe $NUM_OPS $lang-agg.txt > $OUTPATH/$lang.codes
+    bsub -W $TIME \
+                -n $NUM_CPUS \
+                -R "rusage[mem=${CPU_RAM},ngpus_excl_p=${NUM_GPUS}]" \
+                -o logs_cf_data/train_bpe_${lang}.out \
+                $FASTBPE learnbpe $NUM_OPS $lang-agg.txt > $OUTPATH/$lang.codes
     rm $lang-agg.txt
 done
