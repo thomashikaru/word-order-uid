6998039762f2b008818d288ecb4842d625ef9407
Thomas Clark <thclark@mit.edu>
2022-08-09 11:14:18 -0400

    getting unigram frequencies

---
+++ counterfactual/save_unigram_freqs.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+import pandas as pd
+import argparse
+from iso_639 import lang_codes
+from glob import glob
+import os
+from nltk.tokenize import word_tokenize
+
+
+def save_freqs(filename, save_dir, lang):
+    d = defaultdict(int)
+    with open(filename) as f:
+        for line in f:
+            for word in word_tokenize(line):
+                d[word] += 1
+    df = pd.DataFrame.from_records(iter(d.items()), columns=["word", "count"])
+    count_sum = df["count"].sum()
+    df["freq"] = (df["count"] / count_sum).astype("float32")
+    df.sort_values("count", ascending=False, inplace=True)
+    df.to_csv(f"{save_dir}/{lang}.csv", index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--langs",
+        help="comma-separated list of languages codes for which to get unigram freqs",
+        default="en,ru,fr,de,vi,id,hu,tr",
+    )
+    parser.add_argument(
+        "--data_dir", help="directory with language data", default="../raw_data/wiki40b"
+    )
+    parser.add_argument(
+        "--save_dir", help="directory to save freq counts", default="freqs"
+    )
+    args = parser.parse_args()
+
+    langs = args.langs.split(",")
+    for lang in langs:
+        filename = os.path.join(args.data_dir, f"{lang}.tiny")
+        save_freqs(filename, args.save_dir, lang)
