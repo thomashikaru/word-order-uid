1a2654efcfdc3333794013232c30ad4c613ad187
anonymous <thclark@mit.edu>
2023-03-31 15:16:10 +0900

    removed irrelevant files

--- counterfactual/scripts/dep_len.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-LANGUAGE=$1
-MODEL=$2
-DATAFILE=$3
-RESULTSFILE=$4
-SEED=$5
-
-python ../apply_counterfactual_grammar.py --output_dl_only --language $LANGUAGE --model $MODEL --filename $DATAFILE --seed $SEED > $RESULTSFILE
--- counterfactual/scripts/freq_opt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-LANGUAGE=$1
-MODEL=$2
-DATAFILE=$3
-
-python ../apply_counterfactual_grammar.py --language $LANGUAGE --model $MODEL --filename $DATAFILE --freq_opt
